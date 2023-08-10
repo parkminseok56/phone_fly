7162d86ecbf0b60d9bb5b6b3fa072bdbffb1c78a
박민석 <133790403+parkminseok56@users.noreply.github.com>
2023-08-10 16:08:47 +0900

    Update ProductVO.java

--- src/main/java/com/ezen/phonefly2/dto/ProductVO.java
+++ src/main/java/com/ezen/phonefly2/dto/ProductVO.java
@@ -7,16 +7,20 @@ import lombok.Data;
 
 @Data
 public class ProductVO {
-	private Integer pseq;
-	private String name;
-	private Integer price1;
-	private Integer price2;
-	private Integer price3;
-	private String content;
-	private String useyn;
-	private String eventyn;
-	private String bestyn;
-	private Timestamp indate;
-	private String mfc;
-	private List<ColorVO> colorList;
+
+   // 상품 정보를 저장하는 데 사용되며, @Data 어노테이션으로 인해 getter, setter, toString 등의 메서드가 자동으로 생성됩니다.
+	
+    private Integer pseq;     // 상품 일련번호
+    private String name;      // 상품명
+    private Integer price1;   // 상품 가격1
+    private Integer price2;   // 상품 가격2
+    private Integer price3;   // 상품 가격3
+    private String content;   // 상품 내용
+    private String useyn;     // 상품 사용 여부 ("Y" 또는 "N")
+    private String eventyn;   // 이벤트 여부 ("Y" 또는 "N")
+    private String bestyn;    // 베스트 상품 여부 ("Y" 또는 "N")
+    private Timestamp indate; // 상품 등록일시
+    private String mfc;       // 제조사명
+
+    private List<ColorVO> colorList; // 컬러 목록
 }
