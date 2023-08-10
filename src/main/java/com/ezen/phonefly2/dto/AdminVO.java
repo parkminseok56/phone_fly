282f7f1899e143e1a69b59b866a3a3c70e2c9344
박민석 <133790403+parkminseok56@users.noreply.github.com>
2023-08-10 14:36:10 +0900

    Update AdminVO.java

--- src/main/java/com/ezen/phonefly2/dto/AdminVO.java
+++ src/main/java/com/ezen/phonefly2/dto/AdminVO.java
@@ -4,7 +4,10 @@ import lombok.Data;
 
 @Data
 public class AdminVO {
-	private String id;
-	private String pwd;
-	private String name;
+
+    // 관리자의 정보를 저장하는데 사용되며, @Data 어노테이션으로 인해 getter, setter, toString 등의 메서드가 자동으로 생성됩니다.	
+	
+    private String id;   // 관리자 아이디
+    private String pwd;  // 관리자 비밀번호
+    private String name; // 관리자 이름
 }
