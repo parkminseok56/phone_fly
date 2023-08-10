daa2f726cee13cc237f255096929672b5cca6ad3
박민석 <133790403+parkminseok56@users.noreply.github.com>
2023-08-10 14:43:49 +0900

    Update ColorVO.java

--- src/main/java/com/ezen/phonefly2/dto/ColorVO.java
+++ src/main/java/com/ezen/phonefly2/dto/ColorVO.java
@@ -4,9 +4,12 @@ import lombok.Data;
 
 @Data
 public class ColorVO {
-	private int cseq;
-	private int pseq;
-	private String name;
-	private String ccode;
-	private String image;
+
+    // 제품의 색상 정보를 저장하는데 사용되며, @Data 어노테이션으로 인해 getter, setter, toString 등의 메서드가 자동으로 생성됩니다.
+	
+    private int cseq;       // 색상 일련번호
+    private int pseq;       // 제품 일련번호
+    private String name;    // 색상 이름
+    private String ccode;   // 색상 코드
+    private String image;   // 색상 이미지
 }
