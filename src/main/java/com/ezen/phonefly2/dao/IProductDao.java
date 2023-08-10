adccec20195b0735e3859f5419beaede91cc1721
박민석 <133790403+parkminseok56@users.noreply.github.com>
2023-08-10 14:29:40 +0900

    Update IProductDao.java

--- src/main/java/com/ezen/phonefly2/dao/IProductDao.java
+++ src/main/java/com/ezen/phonefly2/dao/IProductDao.java
@@ -12,12 +12,26 @@ import com.ezen.phonefly2.dto.RplanVO;
 @Mapper
 public interface IProductDao {
 
-	ProductVO getProduct(int pseq);
-	List<ProductVO> getMfcList(String mfc);
-	List<RplanVO> getRplanList();
-	int countOrderById(String id, int pseq);
-	ArrayList<ReviewVO> getReviews(int pseq);
-	List<ProductVO> getProductList();
-	void writeReview(ReviewVO rvo);
+    // 제품 정보와 관련된 데이터베이스 연동을 수행하는 메서드들이 선언되어 있습니다. 	
+	
+    // 특정 제품 정보 가져오기
+    ProductVO getProduct(int pseq);
 
+    // 특정 제조사에 해당하는 제품 목록 가져오기
+    List<ProductVO> getMfcList(String mfc);
+
+    // 요금제 목록 가져오기
+    List<RplanVO> getRplanList();
+
+    // 특정 아이디와 제품 번호에 해당하는 주문 수량 가져오기
+    int countOrderById(String id, int pseq);
+
+    // 특정 제품의 리뷰 목록 가져오기
+    ArrayList<ReviewVO> getReviews(int pseq);
+
+    // 모든 제품 목록 가져오기
+    List<ProductVO> getProductList();
+
+    // 리뷰 작성하기
+    void writeReview(ReviewVO rvo);
 }
