044d5d773cb463a525bae24fb67d9e6e9f1e9bdc
박민석 <133790403+parkminseok56@users.noreply.github.com>
2023-08-10 14:52:27 +0900

    Update NoticeVO.java

--- src/main/java/com/ezen/phonefly2/dto/NoticeVO.java
+++ src/main/java/com/ezen/phonefly2/dto/NoticeVO.java
@@ -6,9 +6,12 @@ import lombok.Data;
 
 @Data
 public class NoticeVO {
-	private int nseq;
-	private String id;
-	private String subject;
-	private String content;
-	private Timestamp indate;
+
+   // 공지사항 정보를 저장하는데 사용되며, @Data 어노테이션으로 인해 getter, setter, toString 등의 메서드가 자동으로 생성됩니다. 
+	
+    private int nseq;         // 공지사항 일련번호
+    private String id;        // 작성자 아이디
+    private String subject;   // 공지사항 제목
+    private String content;   // 공지사항 내용
+    private Timestamp indate; // 공지사항 등록일시
 }
