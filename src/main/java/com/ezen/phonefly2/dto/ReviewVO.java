8216fb874d851bda59a84764b02ef53e0834d8e4
박민석 <133790403+parkminseok56@users.noreply.github.com>
2023-08-10 17:00:30 +0900

    Update ReviewVO.java

--- src/main/java/com/ezen/phonefly2/dto/ReviewVO.java
+++ src/main/java/com/ezen/phonefly2/dto/ReviewVO.java
@@ -6,12 +6,15 @@ import lombok.Data;
 
 @Data
 public class ReviewVO {
-	private int rvseq;
-	private String id;
-	private String subject;
-	private String content;
-	private Timestamp indate;
-	private int pseq;
-	private String name;
-	private String pname;
+
+   // 리뷰 정보를 저장하는 데 사용되며, @Data 어노테이션으로 인해 getter, setter, toString 등의 메서드가 자동으로 생성됩니다.	
+	
+    private int rvseq;      // 리뷰 일련번호
+    private String id;      // 작성자 아이디
+    private String subject; // 리뷰 제목
+    private String content; // 리뷰 내용
+    private Timestamp indate; // 리뷰 등록일시
+    private int pseq;       // 상품 번호 [product FK]
+    private String name;    // 작성자 이름 (join으로 가져옴)
+    private String pname;   // 상품명 (join으로 가져옴)
 }
