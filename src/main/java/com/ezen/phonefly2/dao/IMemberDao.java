e6388908edfae8d86e4dfd0d13a0b0762f005a61
박민석 <133790403+parkminseok56@users.noreply.github.com>
2023-08-10 14:20:57 +0900

    Update IMemberDao.java

--- src/main/java/com/ezen/phonefly2/dao/IMemberDao.java
+++ src/main/java/com/ezen/phonefly2/dao/IMemberDao.java
@@ -7,13 +7,29 @@ import com.ezen.phonefly2.dto.MemberVO;
 @Mapper
 public interface IMemberDao {
 
-	MemberVO getMember(String id);
-	void joinKakao(MemberVO mvo);
-	void join(MemberVO mvo);
-	void memberUpdate(MemberVO mvo);
-	void deleteMember(String id);
-	MemberVO findId(String name, String phone);
-	int findMember(String id, String name, String phone);
-	void setNewPwd(String id, String pwd);
+    // 회원 정보와 관련된 데이터베이스 연동을 수행하는 메서드들이 선언되어 있습니다. 
+	
+    // 특정 아이디에 해당하는 회원 정보 가져오기
+    MemberVO getMember(String id);
 
+    // 카카오 회원 가입 처리
+    void joinKakao(MemberVO mvo);
+
+    // 회원 가입 처리
+    void join(MemberVO mvo);
+
+    // 회원 정보 업데이트 처리
+    void memberUpdate(MemberVO mvo);
+
+    // 특정 아이디에 해당하는 회원 정보 삭제
+    void deleteMember(String id);
+
+    // 이름과 휴대폰 번호로 아이디 찾기
+    MemberVO findId(String name, String phone);
+
+    // 아이디, 이름, 휴대폰 번호로 회원 정보 찾기
+    int findMember(String id, String name, String phone);
+
+    // 새로운 비밀번호 설정
+    void setNewPwd(String id, String pwd);
 }
