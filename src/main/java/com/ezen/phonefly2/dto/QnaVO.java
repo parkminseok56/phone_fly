ae86a8a1111d4c09a0dc25d66a9ca3af443332a2
박민석 <133790403+parkminseok56@users.noreply.github.com>
2023-08-10 16:10:44 +0900

    Update QnaVO.java

--- src/main/java/com/ezen/phonefly2/dto/QnaVO.java
+++ src/main/java/com/ezen/phonefly2/dto/QnaVO.java
@@ -6,11 +6,14 @@ import lombok.Data;
 
 @Data
 public class QnaVO {
-	private int qseq;
-	private String id;
-	private String subject;
-	private String content;
-	private String reply;
-	private String rep;
-	private Timestamp indate;
+
+   //  Q&A 정보를 저장하는 데 사용되며, @Data 어노테이션으로 인해 getter, setter, toString 등의 메서드가 자동으로 생성됩니다.	
+	
+    private int qseq;        // Q&A 일련번호
+    private String id;       // 작성자 아이디
+    private String subject;  // Q&A 제목
+    private String content;  // Q&A 내용
+    private String reply;    // 관리자 답변 내용
+    private String rep;      // 답변 여부 ("Y" 또는 "N")
+    private Timestamp indate; // Q&A 등록일시
 }
