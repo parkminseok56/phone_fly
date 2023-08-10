206814d0dccfd2256e1901a826dd338ccd168b02
박민석 <133790403+parkminseok56@users.noreply.github.com>
2023-08-10 14:19:57 +0900

    Update IMainDao.java

--- src/main/java/com/ezen/phonefly2/dao/IMainDao.java
+++ src/main/java/com/ezen/phonefly2/dao/IMainDao.java
@@ -10,9 +10,18 @@ import com.ezen.phonefly2.dto.ProductVO;
 
 @Mapper
 public interface IMainDao {
-	// author : bhs
-	List<ProductVO> getBestList();
-	List<ProductVO> getEventList();
-	List<ColorVO> getColorList(int pseq);
-	List<BannerVO> getBannerList();
+
+    // 메인 페이지와 관련된 데이터베이스 연동을 수행하는 메서드들이 선언되어 있습니다. 
+	
+    // 베스트 상품 목록 가져오기
+    List<ProductVO> getBestList();
+
+    // 이벤트 상품 목록 가져오기
+    List<ProductVO> getEventList();
+
+    // 특정 제품의 모든 색상 목록 가져오기
+    List<ColorVO> getColorList(int pseq);
+
+    // 배너 목록 가져오기
+    List<BannerVO> getBannerList();
 }
