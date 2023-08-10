7de4df1beb360a62e3ccf27dffe9d89610d41a22
박민석 <133790403+parkminseok56@users.noreply.github.com>
2023-08-10 14:45:40 +0900

    Update CommVO.java

--- src/main/java/com/ezen/phonefly2/dto/CommVO.java
+++ src/main/java/com/ezen/phonefly2/dto/CommVO.java
@@ -4,6 +4,10 @@ import lombok.Data;
 
 @Data
 public class CommVO {
-	private int mseq;
-	private String name;
+
+    // 공통적인 데이터 정보를 저장하는데 사용됩니다. @Data 어노테이션으로 인해 getter, setter, toString 등의 메서드가 자동으로 생성됩니다. 
+    // 데이터의 공통 필드를 표현할 때 사용됩니다.
+	
+    private int mseq;    // 일련번호 (멤버 일련번호)
+    private String name; // 이름 (멤버 이름)
 }
