b5c6e791c63cc9e3bbe4af60e33cca416f121755
박민석 <133790403+parkminseok56@users.noreply.github.com>
2023-08-11 14:06:23 +0900

    Update MainService.java

--- src/main/java/com/ezen/phonefly2/service/MainService.java
+++ src/main/java/com/ezen/phonefly2/service/MainService.java
@@ -13,25 +13,30 @@ import com.ezen.phonefly2.dto.ProductVO;
 
 @Service
 public class MainService {
+    
+    @Autowired
+    IMainDao mdao; // IMainDao 인터페이스를 구현한 DAO 클래스를 주입받는 필드
 
-	@Autowired
-	IMainDao mdao;
+    // 메인 페이지에 표시할 상품 및 배너 정보를 가져오는 메서드
+    public void getMainProduct(HashMap<String, Object> result) {
+        List<ProductVO> mainBestList = mdao.getBestList(); // 주요 상품 목록 가져오기
 
-	public void getMainProduct(HashMap<String, Object> result) {
-		// author : bhs
-		List<ProductVO> mainBestList = mdao.getBestList();
-		for (ProductVO pvo : mainBestList) {
-			List<ColorVO> colorList = mdao.getColorList(pvo.getPseq());
-			pvo.setColorList(colorList);
-		}
-		List<ProductVO> mainEventList = mdao.getEventList();
-		for (ProductVO pvo : mainEventList) {
-			List<ColorVO> colorList = mdao.getColorList(pvo.getPseq());
-			pvo.setColorList(colorList);
-		}
-		List<BannerVO> bannerList = mdao.getBannerList();
-		result.put("mainBestList", mainBestList);
-		result.put("mainEventList", mainEventList);
-		result.put("bannerList", bannerList);
-	}
+        for (ProductVO pvo : mainBestList) {
+            List<ColorVO> colorList = mdao.getColorList(pvo.getPseq()); // 상품의 색상 목록 가져오기
+            pvo.setColorList(colorList); // 상품 객체에 색상 목록 설정
+        }
+
+        List<ProductVO> mainEventList = mdao.getEventList(); // 이벤트 상품 목록 가져오기
+
+        for (ProductVO pvo : mainEventList) {
+            List<ColorVO> colorList = mdao.getColorList(pvo.getPseq()); // 상품의 색상 목록 가져오기
+            pvo.setColorList(colorList); // 상품 객체에 색상 목록 설정
+        }
+
+        List<BannerVO> bannerList = mdao.getBannerList(); // 배너 목록 가져오기
+
+        result.put("mainBestList", mainBestList); // 주요 상품 목록을 결과에 저장
+        result.put("mainEventList", mainEventList); // 이벤트 상품 목록을 결과에 저장
+        result.put("bannerList", bannerList); // 배너 목록을 결과에 저장
+    }
 }
