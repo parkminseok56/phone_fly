e181430a3cc7a1362e9ae7505d0b8c8ab6fccd86
박민석 <133790403+parkminseok56@users.noreply.github.com>
2023-08-11 14:05:33 +0900

    Update CommonService.java

--- src/main/java/com/ezen/phonefly2/service/CommonService.java
+++ src/main/java/com/ezen/phonefly2/service/CommonService.java
@@ -16,83 +16,95 @@ import com.ezen.phonefly2.util.Paging;
 
 @Service
 public class CommonService {
-	// 다시작성 : bhs
-	
-	@Autowired
-	ICommonDao cdao;
+    
+    @Autowired
+    ICommonDao cdao; // ICommonDao 인터페이스를 구현한 DAO 클래스를 주입받는 필드
 
-	public void getNoticeList(HashMap<String, Object> result) {
-		HttpServletRequest request = (HttpServletRequest)result.get("request");
-		HttpSession session = request.getSession();
-		int page = 1;
-		session.removeAttribute("page");
-		if (request.getParameter("page") != null) {
-			page = Integer.parseInt(request.getParameter("page"));
-		}
-		Paging paging = new Paging();
-		paging.setPage(page);
-		int count = cdao.getAllCount("notice");
-		paging.setTotalCount(count);
-		session.setAttribute("page", page);
-		List<NoticeVO> noticeList = cdao.getNoticeList(paging);
-		result.put("noticeList", noticeList);
-		result.put("paging", paging);
-	}
+    // 공지사항 목록 조회 메서드
+    public void getNoticeList(HashMap<String, Object> result) {
+        HttpServletRequest request = (HttpServletRequest) result.get("request");
+        HttpSession session = request.getSession();
+        int page = 1;
+        session.removeAttribute("page");
 
-	public NoticeVO getNotice(int nseq) {
-		return cdao.getNotice(nseq);
-	}
+        if (request.getParameter("page") != null) {
+            page = Integer.parseInt(request.getParameter("page"));
+        }
 
-	public void getEventList(HashMap<String, Object> result) {
-		HttpServletRequest request = (HttpServletRequest)result.get("request");
-		HttpSession session = request.getSession();
-		int page = 1;
-		session.removeAttribute("page");
-		if (request.getParameter("page") != null) {
-			page=Integer.parseInt(request.getParameter("page"));
-		}
-		Paging paging = new Paging();
-		paging.setPage(page);
-		int count = cdao.getAllCount("event");
-		paging.setTotalCount(count);
-		session.setAttribute("page", page);
-		List<EventVO> eventList = cdao.getEventList(paging);
-		result.put("eventList", eventList);
-		result.put("paging", paging);
-	}
+        Paging paging = new Paging();
+        paging.setPage(page);
 
-	public EventVO getEvent(int eseq) {
-		return cdao.getEvent(eseq);
-	}
+        int count = cdao.getAllCount("notice"); // "notice" 테이블의 총 개수 가져오기
+        paging.setTotalCount(count);
 
-	public void insertNotice(NoticeVO noticevo) {
-		cdao.insertNotice(noticevo);
-		
-	}
+        session.setAttribute("page", page);
 
-	public void updateNotice(NoticeVO noticeVO) {
-		cdao.updateNotice(noticeVO);
-		
-	}
+        List<NoticeVO> noticeList = cdao.getNoticeList(paging); // 페이징 처리된 공지사항 목록 가져오기
+        result.put("noticeList", noticeList);
+        result.put("paging", paging);
+    }
 
-	public void deleteNotice(int nseq) {
-		cdao.deleteNotice(nseq);		
-	}
+    // 공지사항 내용 조회 메서드
+    public NoticeVO getNotice(int nseq) {
+        return cdao.getNotice(nseq);
+    }
 
-	public void insertEvent(EventVO eventvo) {
-		cdao.insertEvent(eventvo);
-		
-	}
+    // 이벤트 목록 조회 메서드
+    public void getEventList(HashMap<String, Object> result) {
+        HttpServletRequest request = (HttpServletRequest) result.get("request");
+        HttpSession session = request.getSession();
+        int page = 1;
+        session.removeAttribute("page");
 
-	public void updateEvent(EventVO eventvo) {
-		cdao.updateEvent(eventvo);
-		
-	}
+        if (request.getParameter("page") != null) {
+            page = Integer.parseInt(request.getParameter("page"));
+        }
 
-	public void deleteEvent(int eseq) {
-		cdao.deleteEvent(eseq);		
-	}
-	
-	
+        Paging paging = new Paging();
+        paging.setPage(page);
 
+        int count = cdao.getAllCount("event"); // "event" 테이블의 총 개수 가져오기
+        paging.setTotalCount(count);
+
+        session.setAttribute("page", page);
+
+        List<EventVO> eventList = cdao.getEventList(paging); // 페이징 처리된 이벤트 목록 가져오기
+        result.put("eventList", eventList);
+        result.put("paging", paging);
+    }
+
+    // 이벤트 내용 조회 메서드
+    public EventVO getEvent(int eseq) {
+        return cdao.getEvent(eseq);
+    }
+
+    // 공지사항 추가 메서드
+    public void insertNotice(NoticeVO noticevo) {
+        cdao.insertNotice(noticevo);
+    }
+
+    // 공지사항 수정 메서드
+    public void updateNotice(NoticeVO noticeVO) {
+        cdao.updateNotice(noticeVO);
+    }
+
+    // 공지사항 삭제 메서드
+    public void deleteNotice(int nseq) {
+        cdao.deleteNotice(nseq);
+    }
+
+    // 이벤트 추가 메서드
+    public void insertEvent(EventVO eventvo) {
+        cdao.insertEvent(eventvo);
+    }
+
+    // 이벤트 수정 메서드
+    public void updateEvent(EventVO eventvo) {
+        cdao.updateEvent(eventvo);
+    }
+
+    // 이벤트 삭제 메서드
+    public void deleteEvent(int eseq) {
+        cdao.deleteEvent(eseq);
+    }
 }
