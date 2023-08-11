de40003bb222b26a36d9f2adc92d063cb1befc3c
박민석 <133790403+parkminseok56@users.noreply.github.com>
2023-08-11 14:10:51 +0900

    Update QnaService.java

--- src/main/java/com/ezen/phonefly2/service/QnaService.java
+++ src/main/java/com/ezen/phonefly2/service/QnaService.java
@@ -19,44 +19,49 @@ import com.ezen.phonefly2.util.Paging;
 
 @Service
 public class QnaService {
-	// 다시 작성 : bhs
 
-	@Autowired
-	IQnaDao qdao;
+    @Autowired
+    IQnaDao qdao; // IQnaDao 인터페이스를 구현한 DAO 클래스를 주입받는 필드
 
-	public void qnaList(HashMap<String, Object> result) {
-		HttpServletRequest request = (HttpServletRequest)result.get("request");
-		String id = result.get("id").toString();
-		HttpSession session = request.getSession();
-		int page = 1;
-		session.removeAttribute("page");
-		if (request.getParameter("page") != null) {
-			page = Integer.parseInt(request.getParameter("page"));
-		}
-		Paging paging = new Paging();
-		paging.setPage(page);
-		int count = qdao.getAllCountById(id);
-		paging.setTotalCount(count);
-		session.setAttribute("page", page);
-		List<QnaVO> qnaList = qdao.qnaList(paging, id);
-		result.put("qnaList", qnaList);
-		result.put("paging", paging);
-	}
+    // Q&A 목록 조회 및 페이징 처리 메서드
+    public void qnaList(HashMap<String, Object> result) {
+        HttpServletRequest request = (HttpServletRequest) result.get("request");
+        String id = result.get("id").toString(); // 사용자 아이디
+        HttpSession session = request.getSession();
+        int page = 1;
+        session.removeAttribute("page");
+        
+        if (request.getParameter("page") != null) {
+            page = Integer.parseInt(request.getParameter("page"));
+        }
+        
+        Paging paging = new Paging();
+        paging.setPage(page);
+        int count = qdao.getAllCountById(id); // 해당 사용자의 Q&A 총 개수 조회
+        paging.setTotalCount(count);
+        session.setAttribute("page", page);
+        List<QnaVO> qnaList = qdao.qnaList(paging, id); // 해당 사용자의 Q&A 목록 조회
+        result.put("qnaList", qnaList);
+        result.put("paging", paging);
+    }
 
-	public QnaVO getQna(int qseq) {
-		return qdao.getQna(qseq);
-	}
+    // 특정 Q&A 내용 조회 메서드
+    public QnaVO getQna(int qseq) {
+        return qdao.getQna(qseq); // 특정 Q&A 내용 조회
+    }
 
-	public void qnaWrite(QnaVO qvo) {
-		qdao.qnaWrite(qvo);
-	}
+    // Q&A 작성 메서드
+    public void qnaWrite(QnaVO qvo) {
+        qdao.qnaWrite(qvo); // Q&A 작성
+    }
 
-	public void qnaUpdate(QnaVO qvo) {
-		qdao.qnaUpdate(qvo);
-	}
+    // Q&A 수정 메서드
+    public void qnaUpdate(QnaVO qvo) {
+        qdao.qnaUpdate(qvo); // Q&A 수정
+    }
 
-	public void qnaDelete(int qseq) {
-		qdao.qnaDelete(qseq);
-	}
-
+    // Q&A 삭제 메서드
+    public void qnaDelete(int qseq) {
+        qdao.qnaDelete(qseq); // Q&A 삭제
+    }
 }
