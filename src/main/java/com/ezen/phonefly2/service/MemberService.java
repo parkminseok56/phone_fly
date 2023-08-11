56d4a952d8368da938ef8687c657438da4bed737
박민석 <133790403+parkminseok56@users.noreply.github.com>
2023-08-11 14:07:00 +0900

    Update MemberService.java

--- src/main/java/com/ezen/phonefly2/service/MemberService.java
+++ src/main/java/com/ezen/phonefly2/service/MemberService.java
@@ -8,40 +8,47 @@ import com.ezen.phonefly2.dto.MemberVO;
 
 @Service
 public class MemberService {
+    
+    @Autowired
+    IMemberDao mdao; // IMemberDao 인터페이스를 구현한 DAO 클래스를 주입받는 필드
 
-	@Autowired
-	IMemberDao mdao;
+    // 멤버 정보 조회 메서드
+    public MemberVO getMember(String id) {
+        return mdao.getMember(id);
+    }
 
-	public MemberVO getMember(String id) {
-		return mdao.getMember(id);
-	}
+    // 카카오 회원 가입 메서드
+    public void joinKakao(MemberVO mvo) {
+        mdao.joinKakao(mvo);
+    }
 
-	public void joinKakao(MemberVO mvo) {
-		mdao.joinKakao(mvo);
-	}
+    // 일반 회원 가입 메서드
+    public void join(MemberVO mvo) {
+        mdao.join(mvo);
+    }
 
-	public void join(MemberVO mvo) {
-		mdao.join(mvo);
-	}
+    // 멤버 정보 업데이트 메서드
+    public void memberUpdate(MemberVO mvo) {
+        mdao.memberUpdate(mvo);
+    }
 
-	public void memberUpdate(MemberVO mvo) {
-		mdao.memberUpdate(mvo);
-	}
+    // 멤버 삭제 메서드
+    public void deleteMember(String id) {
+        mdao.deleteMember(id);
+    }
 
-	public void deleteMember(String id) {
-		mdao.deleteMember(id);
-	}
+    // 아이디 찾기 메서드
+    public MemberVO findId(String name, String phone) {
+        return mdao.findId(name, phone);
+    }
 
-	public MemberVO findId(String name, String phone) {
-		return mdao.findId(name, phone);
-	}
+    // 회원 정보 확인 메서드
+    public int findMember(String id, String name, String phone) {
+        return mdao.findMember(id, name, phone);
+    }
 
-	public int findMember(String id, String name, String phone) {
-		return mdao.findMember(id, name, phone);
-	}
-
-	public void setNewPwd(String id, String pwd) {
-		mdao.setNewPwd(id, pwd);
-	}
-
+    // 새로운 비밀번호 설정 메서드
+    public void setNewPwd(String id, String pwd) {
+        mdao.setNewPwd(id, pwd);
+    }
 }
